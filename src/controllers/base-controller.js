60e8ab3ee5ec1125307e3310a179f91ed202ee52
enkaypeter <enkaypeter@gmail.com>
2020-07-23 23:24:59 +0100

    :construction_worker: adding cloudbuild file and modifying /api return message

--- src/controllers/base-controller.js
+++ src/controllers/base-controller.js
@@ -4,7 +4,7 @@ module.exports = {
     index: (req, res) => {
         return res.status(200).json({
             success: true,
-            message: "Hi ya! :)",
+            message: "Hey there :)",
         })
     },
 
